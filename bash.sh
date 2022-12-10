d74d6bfc24b8ff2d4536fed4df6473ae9745371c
Betrand Njibamum <107630006+Njibamum@users.noreply.github.com>
2022-12-10 10:37:39 +0100

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+function copyFiles() {
+   arr=("$@")
+   for i in "${arr[@]}";
+      do
+        echo "$i" | sed -e 's/.*\.\(.*\..*\)/\1/' -e 's|^[^/]*//||' -e 's|/.*$||'
+      done
+
+}
+
+array=("http://yahoo.com/" "aws.amazon.com" "tiktok.com") 
+
+copyFiles "${array[@]}"
